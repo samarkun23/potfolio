23d71263604c5622120a5e6de27688d344d1b326
samarkun <samarkun4@gmail.com>
2026-07-04 23:11:23 +0530

    add images

--- next.config.ts
+++ next.config.ts
@@ -1,34 +1,19 @@
 import type { NextConfig } from "next";
-import createMDX from "@next/mdx";
 import remarkGfm from "remark-gfm";
 import rehypeSlug from "rehype-slug";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import rehypePrettyCode from "rehype-pretty-code";
-
-const withMDX = createMDX({
-  options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [
-      rehypeSlug,
-      [rehypeAutolinkHeadings, { behavior: "wrap" }],
-      rehypePrettyCode,
-    ],
-  },
-});
+//import rehypePrettyCode from "rehype-pretty-code";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      { protocol: "https", hostname: "github-readme-stats.vercel.app" },
-      { protocol: "https", hostname: "ghchart.rshah.org" },
-    ],
-  },
-  experimental: {
-    mdxRs: true
-  }
+    output: "export",
+    pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+    images: {
+        unoptimized: true,
+        remotePatterns: [
+            { protocol: "https", hostname: "github-readme-stats.vercel.app" },
+            { protocol: "https", hostname: "ghchart.rshah.org" },
+        ],
+    }
 };
 
-export default withMDX(nextConfig);
+export default nextConfig;
